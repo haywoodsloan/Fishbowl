31e9a7634844bc8d75b9ffee092f0d31168a42ec
haywoodsloan <haywood.sloan@gmail.com>
2015-11-06 20:52:26 -0500

    Replaced alert and confirm with features on page.

--- Fishbowl_Server/web/entries.js
+++ Fishbowl_Server/web/entries.js
@@ -1,11 +1,27 @@
-function submitConfirm(){
-    
-    for (var i = 1; i <= 5; i++){
-        if(document.getElementById("entry" + i).value.trim() === ""){
-            alert("You have not entered all 5 entries. Please Try again.");
+function submitConfirm() {
+
+    for (var i = 1; i <= 5; i++) {
+        if (document.getElementById("entry" + i).value.trim() === "") {
+            document.getElementById("noticeLabel").innerHTML = "You have not entered 5 entries. Please try again.";
+            document.getElementById("noticeLabel").style.visibility = "visible";
+
+            document.getElementById("cancelButton").style.visiblity = "hidden";
             return false;
         }
     }
-    
-    return confirm("Are you ready to submit these entries?");
+
+    if (document.getElementById("cancelButton").style.visibility === "visible") {
+        return true;
+    } else {
+        document.getElementById("cancelButton").style.visibility = "visible";
+        document.getElementById("noticeLabel").style.visibility = "visible";
+
+        document.getElementById("noticeLabel").innerHTML = "Press submit again to confirm submitting these entries.";
+        return false;
+    }
+}
+
+function cancelSubmit() {
+    document.getElementById("noticeLabel").style.visibility = "hidden";
+    document.getElementById("cancelButton").style.visibility = "hidden";
 }
